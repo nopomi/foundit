0efabd6b17b5a69e1842b73474f7dfc6f8b7b44b
Miska Noponen <miska.noponen@gmail.com>
2018-08-25 00:06:09 +0300

    added database models for degree, school and user. Can't get many-to-many relationship to work for favorited degrees.

--- app/models.py
+++ app/models.py
@@ -1,16 +1,35 @@
 from app import db
 
+favorites = db.Table('favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('degree_id', db.Integer, db.ForeignKey('degree.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    favorited = db.relationship("Degree", secondary=favorites)
 
     def __repr__(self):
-        return 'User: {}'.format(self.email)
+        return '<User: {}>'.format(self.email)
+
 
 class School(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.column(db.String(128))
-    #need to add more info here about Schools and other classes
-    #Relationships also need to be specified here
-    #
+    country = db.column(db.String(64))
+    city = db.Column(db.String(64))
+    degrees = db.relationship('Degree', backref='school', lazy='dynamic')
+
+    def __repr__(self):
+        return '<School: {}>'.format(self.name)
+
+class Degree(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.column(db.String(128))
+    description = db.column(db.Text)
+    school_id = db.Column(db.Integer, db.ForeignKey('school.id'))
+
+    def __repr__(self):
+        return '<Degree: {}>'.format(self.name)
