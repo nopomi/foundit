622a260e70811b4b171b13da8d346329029722f2
Miska Noponen <miska.noponen@gmail.com>
2018-08-25 13:41:22 +0300

    added flask shell environment variables and the existing models school, degree, user

--- foundit.py
+++ foundit.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, School, Degree
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'School': School, 'Degree': Degree}
